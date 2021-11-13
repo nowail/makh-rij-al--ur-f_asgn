c75e59295076effbac60c8f32507e1b132b4836f
nowail <91878936+nowail@users.noreply.github.com>
2021-11-13 17:22:55 +0500

    2

---
+++ xml.java
@@ -0,0 +1,14 @@
+package com.example.prac_xml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class xml extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xml_);
+    }
+}
